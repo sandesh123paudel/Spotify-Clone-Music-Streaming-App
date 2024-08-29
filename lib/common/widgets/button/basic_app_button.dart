6b9f7ccca0334092ee0b3c0562c3b323badf17eb
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2024-08-29 07:44:45 +0545

    Choose Mode Page UI Completed

--- lib/common/widgets/button/basic_app_button.dart
+++ lib/common/widgets/button/basic_app_button.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:musicplayer/core/configs/theme/app_colors.dart';
 
 class BasicAppButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String title;
-  const BasicAppButton({super.key, required this.onPressed, required this.title});
+  final double? height;
+  const BasicAppButton(
+      {super.key, required this.onPressed, required this.title, this.height});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed, 
-      style: ElevatedButton.styleFrom(),
-      child: Text(title));
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+            minimumSize: Size.fromHeight(height ?? 80),
+            foregroundColor: AppColors.grey),
+        child: Text(title));
   }
 }
--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:musicplayer/common/widgets/button/basic_app_button.dart';
 import 'package:musicplayer/core/configs/assets/app_images.dart';
 import 'package:musicplayer/core/configs/assets/app_vectors.dart';
 import 'package:musicplayer/core/configs/theme/app_colors.dart';
+import 'package:musicplayer/presentation/choose_mode/pages/choose_mode.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -17,6 +19,10 @@ class GetStartedPage extends StatelessWidget {
             decoration: const BoxDecoration(
                 image: DecorationImage(
                     fit: BoxFit.fill, image: AssetImage(AppImages.introBG))),
+          ),
+          Container(color: Colors.black.withOpacity(0.15)),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
             child: Column(children: [
               Align(
                   alignment: Alignment.topCenter,
@@ -40,9 +46,18 @@ class GetStartedPage extends StatelessWidget {
                     fontSize: 13),
                 textAlign: TextAlign.center,
               ),
+              const SizedBox(height: 20),
+              BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                const ChooseMode()));
+                  },
+                  title: 'Get Started')
             ]),
           ),
-          Container(color: Colors.black.withOpacity(0.15))
         ],
       ),
     );
