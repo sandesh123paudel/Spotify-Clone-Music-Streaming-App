c94496d86e212730ff9ebbe1ea71fd2301150ea2
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-01 22:33:58 +0545

    ..

--- lib/presentation/auth/pages/signin.dart
+++ lib/presentation/auth/pages/signin.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:musicplayer/common/widgets/appbar/app_bar.dart';
-import 'package:musicplayer/common/widgets/button/basic_app_button.dart';
-import 'package:musicplayer/core/configs/assets/app_vectors.dart';
-import 'package:musicplayer/data/models/auth/signin_user_req.dart';
 import 'package:musicplayer/presentation/auth/pages/signup.dart';
-
-import '../../../data/models/auth/create_user_req.dart';
+import 'package:musicplayer/presentation/root/pages/root.dart';
+import '../../../common/widgets/appbar/app_bar.dart';
+import '../../../common/widgets/button/basic_app_button.dart';
+import '../../../core/configs/assets/app_vectors.dart';
+import '../../../data/models/auth/signin_user_req.dart';
 import '../../../domain/usecases/auth/signin.dart';
 import '../../../service_locator.dart';
-import '../../root/pages/root.dart';
 
 class SignInPage extends StatelessWidget {
   SignInPage({super.key});
@@ -20,26 +18,31 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: _signupText(context),
       appBar: BasicAppbar(
-          title: SvgPicture.asset(
-        AppVectors.logo,
-        height: 40,
-        width: 40,
-      )),
-      bottomNavigationBar: _signUpText(context),
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          height: 40,
+          width: 40,
+        ),
+      ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
             _registerText(),
             const SizedBox(
-              height: 30,
+              height: 50,
             ),
             _emailField(context),
-            const SizedBox(height: 20),
+            const SizedBox(
+              height: 20,
+            ),
             _passwordField(context),
-            const SizedBox(height: 20),
+            const SizedBox(
+              height: 20,
+            ),
             BasicAppButton(
                 onPressed: () async {
                   var result = await sl<SigninUseCase>().call(
@@ -47,7 +50,11 @@ class SignInPage extends StatelessWidget {
                           email: _email.text.toString(),
                           password: _password.text.toString()));
                   result.fold((l) {
-                    var snackbar = SnackBar(content: Text(l));
+                    var snackbar = SnackBar(
+                      content: Text(
+                        l),
+                      behavior: SnackBarBehavior.floating,
+                    );
                     ScaffoldMessenger.of(context).showSnackBar(snackbar);
                   }, (r) {
                     Navigator.pushAndRemoveUntil(
@@ -58,8 +65,8 @@ class SignInPage extends StatelessWidget {
                         (route) => false);
                   });
                 },
-                title: 'Sign In'),
-          ]),
+                title: 'Sign In')
+          ],
         ),
       ),
     );
@@ -68,49 +75,47 @@ class SignInPage extends StatelessWidget {
   Widget _registerText() {
     return const Text(
       'Sign In',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 25,
-      ),
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
       textAlign: TextAlign.center,
     );
   }
 
   Widget _emailField(BuildContext context) {
     return TextField(
-        controller: _email,
-        decoration: const InputDecoration(
-          hintText: 'Enter Email',
-        ).applyDefaults(Theme.of(context).inputDecorationTheme));
+      controller: _email,
+      decoration: const InputDecoration(hintText: 'Enter Email')
+          .applyDefaults(Theme.of(context).inputDecorationTheme),
+    );
   }
 
   Widget _passwordField(BuildContext context) {
     return TextField(
-        controller: _password,
-        decoration: const InputDecoration(
-          hintText: 'Password',
-        ).applyDefaults(Theme.of(context).inputDecorationTheme));
+      controller: _password,
+      decoration: const InputDecoration(hintText: 'Password')
+          .applyDefaults(Theme.of(context).inputDecorationTheme),
+    );
   }
 
-  Widget _signUpText(BuildContext context) {
+  Widget _signupText(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text(
-          'Not a member?',
-          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 13),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) => SignUpPage()));
-          },
-          child: const Text('Register Now',
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 13)),
-        ),
-      ]),
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Not A Member? ',
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => SignUpPage()));
+              },
+              child: const Text('Register Now'))
+        ],
+      ),
     );
   }
 }
